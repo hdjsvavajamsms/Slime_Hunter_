62f27b9686f3348a71e41b968a383a6467291f18
hdjsvavajamsms <qkrrbrms6043@gmail.com>
2024-07-19 17:03:52 +0900

    Initial commit

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Monster : MonoBehaviour
+{
+    [SerializeField] private HpBar hpBar;
+    [SerializeField] private Text nameText;
+
+    [SerializeField] private string monsterName;
+    [SerializeField] private float maxHp;
+
+    
+    private float curHp;
+
+    private bool isDead = false;
+    private Animator animator;
+
+    private void Awake()
+    {
+        curHp = maxHp;
+        animator = GetComponent<Animator>();
+        nameText.text = monsterName;
+    }
+
+    public void OnHit(float damage)
+    {
+        curHp -= damage;
+        if (curHp <= 0)
+        {
+            curHp = 0;
+            isDead = true;
+        }
+        animator.SetTrigger("Hit");
+        Debug.Log("Slime Hit!, Current Hp : " + curHp);
+        hpBar.ChangeHpBarAmount(curHp / maxHp);
+        if (isDead)
+        {
+            Debug.Log("Slime is Dead");
+            animator.SetTrigger("Death");
+            Destroy(gameObject,1.5f);
+        }
+    }
+    
+}
